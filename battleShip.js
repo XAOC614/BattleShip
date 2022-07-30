1a1c4405e78cf93bbc5ac8fae1474d4f188eb828
Сергей Фомин <xaoc615@gmail.com>
2022-07-30 13:47:56 +0300

    addObject

--- battleShip.js
+++ battleShip.js
@@ -1,3 +1,17 @@
+let model =  {
+    boardSize : 7,
+    numShips : 3,
+    shipLength :3,
+    shipSunk :0,
+
+
+    ships :[{locations : ["06","16","26"], hits : ["", "", ""]} ,
+            {locations : ["24","34","44"], hits : ["", "", ""]} ,
+            {locations : ["10","11","12"], hits : ["", "", ""]}]
+}
+
+
+
 let viev = {
     displayMessage : function (msg){
         let messageArea = document.getElementById("messageArea")
